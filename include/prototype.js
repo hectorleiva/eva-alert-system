0a17642d4461d814594d79d1b7a18b74ee5b7722
Hector Leiva <hector@hectorleiva.com>
2014-01-16 22:59:47 -0500

    really deleting the library now, including audio mp3 as a notifying trigger that is engaged in jQuery.

--- include/prototype.js
+++ include/prototype.js
@@ -2,17 +2,23 @@
 
 // Load all custom jQuery or Javascript scripts here.
 (function ($) {
-    var eva_notice, eva_alert_attribute, close_btn, notice_text;
+    var eva_notice, eva_alert_attribute, close_btn, notice_text, audio;
+
+    audio = document.createElement('audio');
+    audio.setAttribute('src', '/include/audio/Beep_01.mp3');
 
     $(document).on('click', '.eva-alert', function() {
         $body       = $(document.body);
         eva_notice  = $('#eva-notice');
-        
+
         $body.toggleClass('warning-cover');
         eva_alert_attribute = $(this).attr('eva-notice');
         eva_notice.html('<div class="notice-text">'+eva_alert_attribute+'</div>');
         eva_notice.toggleClass("close open");
         eva_notice.append('<div class="close_button button js-close">CLOSE</div>');
+
+        $.get();
+            audio.play();
     });
 
     $(document).on('click', '.js-close', function() {
